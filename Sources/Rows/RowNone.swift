65c09e6222997529dd1cf7fa1cd424c4af8d9680
Andreas Verhoeven <ave@aveapps.com>
2023-06-07 12:32:09 +0200

    fix

--- Sources/Rows/RowNone.swift
+++ Sources/Rows/RowNone.swift
@@ -10,7 +10,9 @@ import UIKit
 extension Row {
 	/// Use this if you need a statement  in `switch`es that require you to have a row,
 	/// without __any__ content
-	class None: Row<ContainerType, UITableViewCell> {
+	public static var None: Row<ContainerType, UITableViewCell> { NoRows() }
+	
+	private class NoRows: Row<ContainerType, UITableViewCell> {
 		init() {
 			super.init()
 			items.removeAll()
--- Sources/Sections/SectionNone.swift
+++ Sources/Sections/SectionNone.swift
@@ -10,7 +10,9 @@ import Foundation
 extension Section {
 	/// Use this if you need a statement  in `switch`es that require you to have a section,
 	/// without __any__ content
-	class None: Section<ContainerType> {
+	public static var None: Section<ContainerType> { NoSections() }
+	
+	private class NoSections: Section<ContainerType> {
 		init() {
 			super.init(contents: {})
 			items.removeAll()
